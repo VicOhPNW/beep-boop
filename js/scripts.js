6437b33a899b8c1c0a3643e4ce8de4e9c3101062
Victoria Oh <ohvictori@gmail.com>
2018-08-17 10:30:45 -0700

    Create isNotNum function to determine if userInput is or is not a number.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,37 @@
+// --- Back-end logic
+  function convertedNumber(string) {
+    var initialInput = parseInt(string);
+    if (isNotNum(string)) {
+      alert("Please enter a valid number");
+    } else {
+      alert("it works")
+    }
+
+  };
+// --- End of convertedNumber function
 
 
 
 
 
 
-//Front End Logic
+
+// --- Check if string has a number
+  function isNotNum(string) {
+    var regex = /[^0-9]/;
+    return regex.test(string);
+  }
+
+
+// --- Front-end logic
 $(document).ready(function() {
 
   $("#form").submit(function(event) {
     event.preventDefault();
 
     var userNumber = $("#userInput").val();
+    var userOutput = convertedNumber(userNumber)
 
-    $(".userResult").text(userNumber)
+    $(".userResult").text(userOutput)
   });
 });
