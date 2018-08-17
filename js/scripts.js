451bf1be504216d1ca24aed031f06fd336c5aa26
Victoria Oh <ohvictori@gmail.com>
2018-08-17 15:50:03 -0700

    clean up code.

--- js/scripts.js
+++ js/scripts.js
@@ -7,42 +7,36 @@ $(document).ready(function() {
     var userNumber = $("#userInput").val();
     var userOutput = convertedNumber(userNumber)
 
+    function convertedNumber(string) {
+      var initialInput = parseInt(string);
+      if (isNotNum(string)) {
+        alert("Please enter a valid number");
+      } else if ((string.includes(1)) && (string.includes(0)) || (string.includes(1))) {
+        alert("Boop!");
+      } else if (string.includes(0)) {
+        alert("Beep!");
+      } else if (otherNumbers(string)) {
+        $(".userResult").text(resultsArray)
+      }
+    };  // --- End of convertedNumber function
+
     $(".userResult").text(userOutput)
   });
 
-
-
-// --- Back-end logic
-  function convertedNumber(string) {
-    var initialInput = parseInt(string);
-
-    if (isNotNum(string)) {
-      alert("Please enter a valid number");
-    } else if ((string.includes(1)) && (string.includes(0)) || (string.includes(1))) {
-      alert("Boop!");
-    } else if (string.includes(0)) {
-      alert("Beep!");
-    } else if (otherNumbers(string)) {
-      $(".userResult").text(resultsArray)
-    }
-  };  // --- End of convertedNumber function
-
-
 // --- Check if string has a number
-    function isNotNum(string) {
-      var regex = /[^0-9]/;
-      return regex.test(string);
-    }
+      function isNotNum(string) {
+        var regex = /[^0-9]/;
+        return regex.test(string);
+      }
 
 // --- String counts up to given number. Loop works but uncertain how to separate back-end with front-end and/or refactor.
-  function otherNumbers(string) {
-    var resultsArray = [];
-    for (var index = 0; index <= string; index += 1) {
-    resultsArray.push(index);
-    $(".userResult").text(resultsArray)
-    }
-  }
-
+      function otherNumbers(string) {
+        var resultsArray = [];
+        for (var index = 0; index <= string; index += 1) {
+        resultsArray.push(index);
+        $(".userResult").text(resultsArray)
+        }
+      }
 });
 
 // // --- Check if string has letters. Not using, was part of test.
